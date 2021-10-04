fe67b780c5da439b0f4f531c1462edcec1553f9e
PaulLeung93 <78509075+PaulLeung93@users.noreply.github.com>
2021-10-03 23:28:41 -0400

    Added Twitter Handle view. Added Timestamp for each Tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -6,14 +6,16 @@ import org.json.JSONObject;
 public class User {
 
     public String name;
-    public String screenName;
+    public String screenName="@";
     public String profileImageUrl;
 
+    //Name is the Twitter handle, while Screen name is the user's Full Name
     public static User fromJson(JSONObject jsonObject) throws JSONException {
         User user = new User();
         user.name = jsonObject.getString("name");
-        user.screenName = jsonObject.getString("screen_name");
+        user.screenName += jsonObject.getString("screen_name");
         user.profileImageUrl = jsonObject.getString("profile_image_url_https");
+
         return user;
     }
 
